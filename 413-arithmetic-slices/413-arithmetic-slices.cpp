8da78f162a6592ee7220831e48bb4f4b791ac265
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-03-03 12:26:50 +0530

    Time: 3 ms (66.63%), Space: 7.2 MB (99.37%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int diff=0;
-        unordered_map<int,int> mp;
         if(nums.size()<3)return 0;
         diff=nums[0]-nums[1];
         int st=0;
+        int ans=0;
         for(int i=2;i<nums.size();i++)
         {
             if(nums[i-1]-nums[i]==diff)
             {
-                mp[diff]+=i-st-1;
+                ans+=i-st-1;
             }
             else
             {
                 st=i-1;
                 diff=nums[st]-nums[i];
             }
-        }
-        int ans=0;
-        for(auto &i:mp)
-        {
-            ans+=i.second;
         }
         return ans;
     }
