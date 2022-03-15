404d4945178b330e24e766cc42c2c1f38f4e3f8f
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-03-15 17:13:55 +0530

    Time: 1102 ms (83.49%), Space: 277.6 MB (84.75%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -11,34 +11,58 @@
  */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& arr) {
-        unordered_map<int,TreeNode*> nodes;
-        unordered_map<int,bool> check;
-        for(auto &i:arr)
-        {
-            if(nodes.find(i[0])==nodes.end())
-            {
-                nodes[i[0]]=new TreeNode(i[0]);
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> getNode;                          //to check if node alredy exist
+        unordered_map<int, bool> isChild;                               //to check if node has parent or not
+        for(auto &v: descriptions){
+            if(getNode.count(v[0])==0){
+                TreeNode* par = new TreeNode(v[0]);
+                getNode[v[0]] = par;
             }
+            if(getNode.count(v[1])==0){
+                TreeNode* child = new TreeNode(v[1]);
+                getNode[v[1]] = child;
+            }
+            if(v[2]==1) getNode[v[0]]->left = getNode[v[1]];               //left-child
+            else getNode[v[0]]->right = getNode[v[1]];                     //right-child
+            isChild[v[1]] = true;
+        }
+        TreeNode* ans = NULL;
+        for(auto &v: descriptions){
+            if(isChild[v[0]] != true){                  //if node has no parent then this is root node
+                ans = getNode[v[0]]; 
+                break;
+            }
+        }
+        return ans;
+    
+//         unordered_map<int,TreeNode*> nodes;
+//         unordered_map<int,bool> check;
+//         for(auto &i:arr)
+//         {
+//             if(nodes.find(i[0])==nodes.end())
+//             {
+//                 nodes[i[0]]=new TreeNode(i[0]);
+//             }
             
-            if(nodes.find(i[1])==nodes.end())
-            {
-                nodes[i[1]]=new TreeNode(i[1]);
-            }
-            if(i[2]==1)
-            {
-                nodes[i[0]]->left=nodes[i[1]];      
-            }else
-            {
-                nodes[i[0]]->right=nodes[i[1]];    
-            }
-            check[i[1]]=true;
-        }
-        for(auto &i:nodes)
-        {
-            if(check[i.first]!=true)return nodes[i.first];
-        }
-        return new TreeNode(-1);
+//             if(nodes.find(i[1])==nodes.end())
+//             {
+//                 nodes[i[1]]=new TreeNode(i[1]);
+//             }
+//             if(i[2]==1)
+//             {
+//                 nodes[i[0]]->left=nodes[i[1]];      
+//             }else
+//             {
+//                 nodes[i[0]]->right=nodes[i[1]];    
+//             }
+//             check[i[1]]=true;
+//         }
+//         for(auto &i:nodes)
+//         {
+//             if(check[i.first]!=true)return nodes[i.first];
+//         }
+//         return new TreeNode(-1);
     }
 };
 
