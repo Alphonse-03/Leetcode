1e000eac19dbcf11664888a87aa74bb56274f64f
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-03-25 15:13:44 +0530

    Time: 5 ms (61.39%), Space: 7.8 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,28 +1,65 @@
 class Solution {
 public:
-    int dp[51][51];
-    int helper(vector<vector<int>>& costs,int i,int a,int b,int n)
-    {
-        if(i>=costs.size())return 0;
-        if(dp[a][b]!=-1)return dp[a][b];
-        if(a==n)
-        {
-            return dp[a][b]=costs[i][1]+helper(costs,i+1,a,b+1,n);
-        }
-        if(b==n)
-        {
-            return dp[a][b]=costs[i][0]+helper(costs,i+1,a+1,b,n);
-        }
-        int x=costs[i][1]+helper(costs,i+1,a,b+1,n);
-        int y=costs[i][0]+helper(costs,i+1,a+1,b,n);
-        return dp[a][b]=min(x,y);
-            
-    }
-    
-    
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        memset(dp,-1,sizeof(dp));
-        return helper(costs,0,0,0,costs.size()/2);
+        sort(costs.begin(),costs.end(),[](auto &a,auto &b)
+             {
+                 return a[0]-a[1]<b[0]-b[1];
+             });
+        int ans=0;
+        int n=costs.size()/2;
+        for(int i=0;i<costs.size()/2;i++)
+        {
+            ans+=costs[i][0];
+            ans+=costs[n+i][1];
+        }
+        return ans;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     int dp[51][51];
+//     int helper(vector<vector<int>>& costs,int i,int a,int b,int n)
+//     {
+//         if(i>=costs.size())return 0;
+//         if(dp[a][b]!=-1)return dp[a][b];
+//         if(a==n)
+//         {
+//             return dp[a][b]=costs[i][1]+helper(costs,i+1,a,b+1,n);
+//         }
+//         if(b==n)
+//         {
+//             return dp[a][b]=costs[i][0]+helper(costs,i+1,a+1,b,n);
+//         }
+//         int x=costs[i][1]+helper(costs,i+1,a,b+1,n);
+//         int y=costs[i][0]+helper(costs,i+1,a+1,b,n);
+//         return dp[a][b]=min(x,y);
+            
+//     }
+    
+    
+    
+//     int twoCitySchedCost(vector<vector<int>>& costs) {
+//         memset(dp,-1,sizeof(dp));
+//         return helper(costs,0,0,0,costs.size()/2);
+//     }
 };
