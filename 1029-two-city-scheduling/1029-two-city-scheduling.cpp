16abb889afec2a1a8e466f2402a9657deccb6882
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-03-25 15:08:22 +0530

    Time: 4 ms (78.75%), Space: 7.8 MB (94.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int dp[101][51][51];
+    int dp[51][51];
     int helper(vector<vector<int>>& costs,int i,int a,int b,int n)
     {
         if(i>=costs.size())return 0;
-        if(dp[i][a][b]!=-1)return dp[i][a][b];
+        if(dp[a][b]!=-1)return dp[a][b];
         if(a==n)
         {
-            return dp[i][a][b]=costs[i][1]+helper(costs,i+1,a,b+1,n);
+            return dp[a][b]=costs[i][1]+helper(costs,i+1,a,b+1,n);
         }
         if(b==n)
         {
-            return dp[i][a][b]=costs[i][0]+helper(costs,i+1,a+1,b,n);
+            return dp[a][b]=costs[i][0]+helper(costs,i+1,a+1,b,n);
         }
         int x=costs[i][1]+helper(costs,i+1,a,b+1,n);
         int y=costs[i][0]+helper(costs,i+1,a+1,b,n);
-        return dp[i][a][b]=min(x,y);
+        return dp[a][b]=min(x,y);
             
     }
     
