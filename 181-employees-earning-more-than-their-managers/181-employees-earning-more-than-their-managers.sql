6e4b9ca2a28ce7856a354711b76a9f6a77bae45f
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-04 22:48:33 +0530

    Time: 311 ms (90.69%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select name as Employee from employee as e
-where (select 1 from employee where
+where exists(select 1 from employee where
             id=e.managerId and e.salary>salary)
