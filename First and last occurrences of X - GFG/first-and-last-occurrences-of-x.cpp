4d9e86f04d8783b754d79a951dfbc685611c7811
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-07 09:15:59 +0530

    Added solution - LeetHub

--- First and last occurrences of X - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of X - GFG/first-and-last-occurrences-of-x.cpp
@@ -7,11 +7,48 @@ class Solution {
   public:
     vector<int> firstAndLast(vector<int> &arr, int n, int x) {
         // Code here
-        int s=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
-        if(s>n || arr[s]!=x)return {-1};
-        int e=upper_bound(arr.begin(),arr.end(),x)-arr.begin();
-        e-=1;
-        return {s,e};
+        int s=0;
+        int e=n-1;
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+            if(mid>0 && arr[mid]==x && arr[mid-1]!=x)
+            {
+                s=mid;
+                break;
+            }
+            if(arr[mid]>=x)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        if(arr[s]!=x)return {-1};
+        int a=s;
+        e=n-1;
+        s=0;
+        int res=0;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(arr[mid]<x)
+            {
+                s=mid+1;
+            }
+            else if(arr[mid]>x)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                res=mid;
+                s=mid+1;
+            }
+        }
+        return {a,res};
         
     }
 };
