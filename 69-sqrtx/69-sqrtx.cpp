31e0d4fe18c899e5504e744c408ddfe458e1ecb9
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-17 22:18:22 +0530

    Time: 4 ms (55.94%), Space: 5.8 MB (75.17%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=0;
+        int e=x;
+        while(s<=e)
+        {
+            long long mid=s+(e-s)/2;
+            if(mid*mid==x)return mid;
+            
+            if(mid*mid>x)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+        }
+        return e;
+    }
+};
