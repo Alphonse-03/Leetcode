facd1c96a3b25fd576f3d6c6da1403663928fd17
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-20 00:42:24 +0530

    Time: 3 ms (79.75%), Space: 5.8 MB (95.63%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-  int arrangeCoins(int n) {
-return floor(-0.5+sqrt((double)2*n+0.25));
-}
+    
+    bool poss(long long n,int k)
+    {
+        if(n*(n+1)/2>k)return false;
+        return true;
+    }
+    
+    
+    int arrangeCoins(int n) {
+        int s=1;
+        int e=n;
+        while(s<=e)
+        {
+            long long mid=s+(e-s)/2;
+            if(poss(mid,n))s=mid+1;
+            else e=mid-1;
+        }
+        return e;
+    }
 };
