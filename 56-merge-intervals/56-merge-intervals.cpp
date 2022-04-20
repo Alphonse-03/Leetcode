e410b3326dfa3aa619c8647693e5d74cccda2ba1
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-20 23:30:44 +0530

    Time: 49 ms (54.19%), Space: 18.9 MB (85.41%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,8 +4,7 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end(),[&](auto &a,auto &b)
              {
-                if(a[0]!=b[0]) return a[0]<b[0];
-                 else return a[1]<b[1];
+               return a[0]<b[0];
              });
         int s=intervals[0][0];
         int e=intervals[0][1];
