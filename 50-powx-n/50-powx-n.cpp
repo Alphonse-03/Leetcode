8c2fc56815dae73649c615de4a0974d631ab4283
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-22 01:47:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.03%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     
-    double helper(double x,long long p)
+    double helper(double x,int p)
     {
         if(p==0)return 1;
-        //if(p==1)return x;
         if(p%2==0)return helper(x*x,p/2);
         else return x*helper(x*x,p/2);
     }
-
-    
-    
     double myPow(double x, int n) {
-        long long p=n;
-        double ans= helper(x,p);
+        double ans= helper(x,n);
         if(n<0)return 1/ans;
         return ans;
     }
