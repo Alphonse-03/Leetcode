98062bac7614a62beaed8afb04b0b3be5f83abe4
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-21 23:01:16 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -13,11 +13,11 @@ class Solution{
 
 long long ans=0;
 
-/*void merge(long long* arr,int s,int mid,int e)
+void merge(long long* arr,int s,int mid,int e)
 {
-	int n1=mid-s+1;
-	int n2=e-mid;
-	int left[n1],right[n2];
+	long long  n1=mid-s+1;
+	long long n2=e-mid;
+	long long left[n1],right[n2];
 	for(int i=0;i<n1;i++)
 	{
 		left[i]=arr[i+s];
@@ -26,61 +26,27 @@ long long ans=0;
 	{
 		right[i]=arr[i+mid+1];
 	}
-	int s1=0;
-	int s2=0;
+	int i=0;
+	int j=0;
 	int k=s;
 
-	while(s1<n1 && s2<n2)
+	while(i<n1 && j<n2)
 	{
-		if(left[s1]<=right[s2])
+		if(left[i]<=right[j])
 		{
-			arr[k]=left[s1];
-			s1++;
-			k++;
+			arr[k++]=left[i++];
 		}
 		else
 		{
-			ans+=n1-s1;
-			arr[k]=right[s2];
-			s2++;
-			k++;
+			ans+=n1-i;
+			arr[k++]=right[j++];
 		}
 	}
-	while(s1<n1)arr[k++]=left[s1++];
-	while(s2<n2)arr[k++]=right[s2++];
+	while(i<n1)arr[k++]=left[i++];
+	while(j<n2)arr[k++]=right[j++];
 	
 	
-}*/
-typedef long long ll;
-void merge(ll arr[], ll low, ll mid, ll high)
-    {
-        ll n1 = mid-low+1;
-        ll n2 = high-mid;
-        ll left[n1], right[n2];
-        
-        for(int i=0;i<n1;i++)
-           left[i] = arr[low+i];
-        
-        for(int i=0;i<n2;i++)
-           right[i] = arr[mid+1+i];
-        
-        ll i=0,j=0,k=low;
-        
-        while(i<n1&&j<n2)
-        {
-            if(left[i]<=right[j])
-              arr[k++] = left[i++];
-            else
-            {
-                ans+=(n1-i);
-                arr[k++] = right[j++];
-            }
-        }
-        
-        while(i<n1) arr[k++] = left[i++];
-        while(j<n2) arr[k++] = right[j++];
-        
-    }
+}
 
 void mergeSort(long long *arr,int s,int e)
 {
