9691097d4116662d8bb8b6ad817040a3996a2c2f
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-04-30 22:30:50 +0530

    Time: 569 ms (100.00%), Space: 154 MB (50.00%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,43 +1,92 @@
 class Solution {
 public:
-    int countUnguarded(int m, int n, vector<vector<int>>& g, vector<vector<int>>& w) {
-          vector<vector<int>> v(m, vector<int> (n,0));
-        int k = w.size();
-        for(int i=0;i<k;i++){
-            int x = w[i][0], y = w[i][1];
-            v[x][y] = -2;
-        }
-        k = g.size();
-        for(int i=0;i<k;i++){
-            int x = g[i][0], y = g[i][1];
-            v[x][y] = 2;
-        }
-        for(int j=0;j<k;j++){
-            int x = g[j][0], y = g[j][1];
-            for(int i=x-1;i>=0;i--){ // up
-                if(v[i][y]==-2 || v[i][y]==2) break;
-                v[i][y] = 1;
-            }
-            for(int i=x+1;i<m;i++){ // down
-                if(v[i][y]==-2 || v[i][y]==2) break;
-                v[i][y] = 1;
-            }
-            for(int i=y-1;i>=0;i--){ // left
-                if(v[x][i]==-2 || v[x][i]==2) break;
-                v[x][i] = 1;
-            }
-            for(int i=y+1;i<n;i++){ // right
-                if(v[x][i]==-2 || v[x][i]==2) break;
-                v[x][i] = 1;
-            }
-        }
-        int ans = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!v[i][j]) ans++;
-            }
-        }
-        return ans;
     
+    // guard 1
+    // wall 2
+    // vis 3;
+    
+    
+    
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> grid(m,vector<int>(n,0));
+                                 
+//         set<pair<int,int>> w;
+        
+//         for(auto &i:walls){
+//             w.insert({i[0],i[1]});
+//         }
+        for(auto &i:walls){
+            grid[i[0]][i[1]]=2;
+        }
+               
+          for(auto &i:guards){
+            grid[i[0]][i[1]]=1;
+        }
+                                 
+                                 
+                                 
+                                 
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 pair<int,int> p={i,j};
+//                 if(w.find(p)!=w.end())
+//                 {
+//                     grid[i][j]=2;
+//                 }
+//             }
+//         }
+                                 
+        for(auto &it:guards){
+            int x=it[0];
+            int y=it[1];
+            grid[x][y]=1;
+            for(int i=x+1;i<m;i++)
+            {
+                if(grid[i][y]==2 || grid[i][y]==1)break;
+                else grid[i][y]=3;
+            }
+            for(int i=y+1;i<n;i++)
+            {
+                if(grid[x][i]==2 || grid[x][i]==1)break;
+                else grid[x][i]=3;
+            }
+            for(int i=x-1;i>=0;i--)
+            {
+                if(grid[i][y]==2 || grid[i][y]==1)break;
+                else grid[i][y]=3;
+            }
+            for(int i=y-1;i>=0;i--)
+            {
+                if(grid[x][i]==2 || grid[x][i]==1)break;
+                else grid[x][i]=3;
+            }
+        }
+        
+        int c=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==0)c++;
+            }
+        }
+       
+        
+        
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 cout<<grid[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
+                return c;                  
+                    
     }
 };
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
