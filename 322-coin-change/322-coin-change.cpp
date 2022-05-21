849933855eceda6b1f8ac2577c98815a2a250464
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-05-21 21:48:55 +0530

    Time: 169 ms (24.37%), Space: 12 MB (74.09%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,14 +3,12 @@ public:
     int dp[13][10001];
     int helper(vector<int>& coins,int amount,int i){
         if(amount==0)return 0;
-        if(i==coins.size())return 100000;
+        if(i==coins.size() || amount<0)return 100000;
         if(dp[i][amount]!=-1)return dp[i][amount];
-        if(amount>=coins[i]){
-            return dp[i][amount]=min({1+helper(coins,amount-coins[i],i),1+helper(coins,amount-coins[i],i+1),helper(coins,amount,i+1)});
-        }
-        else{
-            return dp[i][amount]=helper(coins,amount,i+1);
-        }
+       
+            return dp[i][amount]=min({1+helper(coins,amount-coins[i],i),helper(coins,amount,i+1)});
+        
+        
     }
     
     
