66e3a8aa96c8fe12daecdef7369a37fcd0501193
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-05-27 18:57:08 +0530

    Time: 16 ms (72.50%), Space: 10.7 MB (72.16%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& graph,vector<int>& temp,int i,vector<vector<int>>& ans){
-        if(i==graph.size()-1){
-            temp.push_back(i);
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(i);
-        for(auto &j:graph[i]){
-            helper(graph,temp,j,ans);
-            temp.pop_back();
-        }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>>paths;
+        vector<int>path;
+        path.push_back(0);
+        backtrack(paths, graph, path, 0);
         
+        return paths;
     }
     
-    
-    
-    
-    
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        helper(graph,temp,0,ans);
-        return ans;
+    void backtrack(vector<vector<int>>&paths, vector<vector<int>>&graph, vector<int>& path, int cur){
+        int n = graph.size() - 1;
+        if(cur==n){
+         paths.push_back(path);
+            return;
+        }
+        
+        for(auto node: graph[cur]){
+           // add node to solution
+            path.push_back(node);
+            //backtrack
+            backtrack(paths, graph, path, node);
+            //explore other solution candidates
+            path.pop_back();
+        }
     }
 };
