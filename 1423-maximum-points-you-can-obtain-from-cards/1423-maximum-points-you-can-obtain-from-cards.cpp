1d078ff6241adc1b51900f1d1ef6318291280fcb
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-06-26 19:47:42 +0530

    Time: 74 ms (75.12%), Space: 42.5 MB (48.14%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxScore(vector<int>& card, int k) {
+        long long sum=accumulate(card.begin(),card.end(),(long long)0);
+        long long winsum=accumulate(card.begin(),card.begin()+k,(long long)0);
+        long long not_included=sum-winsum;
+        int n=card.size()-1;
+        for(int i=k-1;i>=0;i--){
+            not_included+=card[i];
+            not_included-=card[n];
+            n--;
+            winsum=max(winsum,sum-not_included);
+        }
+        return winsum;
+    }
+};
