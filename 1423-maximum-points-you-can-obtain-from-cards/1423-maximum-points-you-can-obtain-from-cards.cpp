640de3f8c73cd4fb4ac6a50ca4af7314db249715
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-06-26 19:49:24 +0530

    Time: 63 ms (93.08%), Space: 42.5 MB (26.19%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,11 +4,10 @@ public:
         long long sum=accumulate(card.begin(),card.end(),(long long)0);
         long long winsum=accumulate(card.begin(),card.begin()+k,(long long)0);
         long long not_included=sum-winsum;
-        int n=card.size()-1;
+        int n=card.size();
         for(int i=k-1;i>=0;i--){
             not_included+=card[i];
-            not_included-=card[n];
-            n--;
+            not_included-=card[--n];
             winsum=max(winsum,sum-not_included);
         }
         return winsum;
