8baeeb1983e328ff91a4ef7dce73c9a33472b2b2
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-06-29 22:58:04 +0530

    Time: 275 ms (24.43%), Space: 12 MB (63.86%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,42 +1,13 @@
 class Solution {
 public:
-    // Input: vec = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
-    
-    
-    //temp=[[4,0],[5,0],[6,0]]
-    
-    // Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
-    
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& vec) {
-        vector<vector<int>> ans;
-        sort(vec.begin(),vec.end(),[&](auto &a,auto &b){
-            if(a[0]==b[0])return a[1]<b[1];
-            else return a[0]>b[0];
-        });
-      
-        for(auto &i:vec){
-            int dem=i[1];  
-            int j=0;
-            while(dem && j<ans.size()){
-                if(ans[j][0]>=i[0])dem--;
-                j++;
-            
-            } 
-            if(ans.size()==0){
-                ans.push_back(i);
-            }
-            else{
-             
-                ans.insert(ans.begin()+j,i);
-            }
-            
-        }
-        
-        
-        
-        
-        
-        
-        return ans;
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+         auto comp = [](auto& p1, auto& p2)
+                    { return p1[0] > p2[0] || (p1[0] == p2[0] && p1[1] < p2[1]); };
+        sort(people.begin(), people.end(), comp);
+        vector<vector<int>> res;
+        for (auto& p : people) 
+            res.insert(res.begin() + p[1], p);
+        return res;
+     
     }
 };
