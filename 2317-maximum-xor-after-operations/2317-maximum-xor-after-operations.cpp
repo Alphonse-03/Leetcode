df49b4be0ab2a571b393ed863fb9d7edf1947ba0
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-06-30 01:57:11 +0530

    Time: 164 ms (53.14%), Space: 48.2 MB (8.16%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
+        vector<bool> bit(32,false);
+        for(auto &i:nums){
+            int idx=0;
+            while(i){
+                if(i&1)bit[idx]=true;
+                idx++;
+                i/=2;
+            }
+        }
         int ans=0;
-        for(auto &i:nums)ans|=i;
+        for(int i=0;i<32;i++){
+            if(bit[i])ans+=pow(2,i);
+        }
         return ans;
     }
 };
