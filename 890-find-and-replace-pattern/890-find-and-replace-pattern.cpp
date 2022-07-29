ba16a5e82af3dddf7ae64f94661f528f016a33a1
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-07-29 23:57:56 +0530

    Time: 16 ms (7.40%), Space: 8.9 MB (5.07%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,53 +1,65 @@
 class Solution {
 public:
-    vector<pair<int,int>> helper(string& str){
-        vector<pair<int,int>> patt;
-        unordered_map<char,int> mp;
-        int x=0;
-        int c=1;
-        int i=0;
+//     vector<pair<int,int>> helper(string& str){
+//         vector<pair<int,int>> patt;
+//         unordered_map<char,int> mp;
+//         int x=0;
+//         int c=1;
+//         int i=0;
         
-        while(i<str.size()){
-            if(mp.find(str[i])==mp.end()){
-                mp[str[i]]=c;
-                char x=str[i];
-                int count=0;
-                int t=i;
-                while(i<str.size() && str[i]==x){
-                    count++;
-                    i++;
-                }
-                patt.push_back({c,count});
-                mp[str[t]]=c;
-                c++;
-            }
-            else{
-                int a=mp[str[i]];
-                char x=str[i];
-                int count=0;
-                while(i<str.size() && str[i]==x){
-                    count++;
-                    i++;
-                }
-                patt.push_back({a,count});
+//         while(i<str.size()){
+//             if(mp.find(str[i])==mp.end()){
+//                 mp[str[i]]=c;
+//                 char x=str[i];
+//                 int count=0;
+//                 int t=i;
+//                 while(i<str.size() && str[i]==x){
+//                     count++;
+//                     i++;
+//                 }
+//                 patt.push_back({c,count});
+//                 mp[str[t]]=c;
+//                 c++;
+//             }
+//             else{
+//                 int a=mp[str[i]];
+//                 char x=str[i];
+//                 int count=0;
+//                 while(i<str.size() && str[i]==x){
+//                     count++;
+//                     i++;
+//                 }
+//                 patt.push_back({a,count});
                 
-            }
-        }
+//             }
+//         }
         
         
-        return patt;
+//         return patt;
+//     }
+    
+    
+    
+    
+    
+//     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+//         vector<string> ans;
+//         vector<pair<int,int>> m=helper(pattern);
+//         for(auto &i:words){
+//             if(helper(i)==m)ans.push_back(i);
+//         }
+//         return ans;
+//     }
+      vector<string> findAndReplacePattern(vector<string> words, string p) {
+        vector<string> res;
+        for (string w : words) if (F(w) == F(p)) res.push_back(w);
+        return res;
     }
-    
-    
-    
-    
-    
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        vector<string> ans;
-        vector<pair<int,int>> m=helper(pattern);
-        for(auto &i:words){
-            if(helper(i)==m)ans.push_back(i);
-        }
-        return ans;
+
+    string F(string w) {
+        unordered_map<char, int> m;
+        for (char c : w) if (!m.count(c)) m[c] = m.size();
+        for (int i = 0; i < w.length(); ++i) w[i] = 'a' + m[w[i]];
+        return w;
     }
 };
