926da73522b9d074574a046fbec509fd317f9798
Alphonse-03 <46101646+Alphonse-03@users.noreply.github.com>
2022-07-29 23:54:34 +0530

    Time: 8 ms (44.61%), Space: 8.6 MB (43.97%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<pair<int,int>> helper(string& str){
+        vector<pair<int,int>> patt;
+        unordered_map<char,int> mp;
+        int x=0;
+        int c=1;
+        int i=0;
+        
+        while(i<str.size()){
+            if(mp.find(str[i])==mp.end()){
+                mp[str[i]]=c;
+                char x=str[i];
+                int count=0;
+                int t=i;
+                while(i<str.size() && str[i]==x){
+                    count++;
+                    i++;
+                }
+                patt.push_back({c,count});
+                mp[str[t]]=c;
+                c++;
+            }
+            else{
+                int a=mp[str[i]];
+                char x=str[i];
+                int count=0;
+                while(i<str.size() && str[i]==x){
+                    count++;
+                    i++;
+                }
+                patt.push_back({a,count});
+                
+            }
+        }
+        
+        
+        return patt;
+    }
+    
+    
+    
+    
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        vector<pair<int,int>> m=helper(pattern);
+        // auto back=helper(pattern);
+        // for(auto &i:back){
+        //     cout<<i.first<<" "<<i.second<<endl;
+        // }
+        for(auto &i:words){
+            if(helper(i)==m)ans.push_back(i);
+        }
+        return ans;
+    }
+};
